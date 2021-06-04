511c90de855b202c72df1d9580717673d534f3b7
Midhlaj2006 <midlajkalathil2006@gmail.com>
2021-06-04 12:56:07 +0000

    Done

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ coin1 =np.array([[1, 15, 41, 60, 59, 90, 72, 63, 52, 70, 100, 156],[1, 20, 30, 6
 data1 =pd.DataFrame(coin1, index=["CoinA", "CoinB"]).T
 data1.head()
 
-
 # %%
-plot =sb.lineplot(data=data1, palette="magma",markers=True)
-st.pyplot()
-
-
-# %%
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Crypto-currency value")
+st.markdown("Choose the _coin_ and _date_, and see the output by clicking ``See Value`` or get the graph by clicking ``View graph``")
+
 coin =st.selectbox("choose the coin :", list(["CoinA", "CoinB"]))
-date =st.slider("choose the month :", min_value=0, max_value=12, value=5, step=1)
+date =st.slider("choose the date :", min_value=0, max_value=12, value=5, step=1)
 tell =st.button("See Value")
+stat =st.button("View graph")
 
 # %%
 if tell:
     st.text(f"value => {list(data1[str(coin)])[date]}")
-    st.pyplot(plot)
+if stat:
+    plot =sb.lineplot(data=data1, palette="magma",markers=True)
+    st.pyplot()
 
 # %%
